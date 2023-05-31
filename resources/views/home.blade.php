a09573a1fcc9fa0b694f4ba461814c86d5dc8eaf
alfito234 <alfito.devaga@gmail.com>
2023-05-31 10:29:52 +0700

    fix extra space cause of using aos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 
 @section('container')
     @include('partials.navbar')
-    <main class="isolate bg-[#2B2B2B]">
+    <main class="isolate bg-[#2B2B2B] overflow-x-hidden ">
         {{-- <section id="home" class="bg-white pt-36 pb-14 lg:pt-0">
             <div class="container">
                 <div class="flex flex-wrap lg:px-5">
@@ -56,7 +56,7 @@
             <div class="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32"></div>
         </div> --}}
 
-        <!-- Background -->
+        <!-- Hero Section -->
         <div class="relative isolate overflow-hidden pt-14">
             <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
                 aria-hidden="true">
@@ -64,7 +64,7 @@
                     style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
                 </div>
             </div>
-            <div class="flex flex-wrap py-10 lg:px-5 lg:py-0">
+            <div class="flex flex-wrap px-5 py-10 lg:px-5 lg:py-0">
                 <div class="w-full self-center px-10 lg:w-1/2" data-aos="fade-right" data-aos-duration="500"
                     data-aos-easing="ease-in-out">
                     <h2 class="text-2xl font-semibold leading-relaxed text-white">Welcome to</h2>
