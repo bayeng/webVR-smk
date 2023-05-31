970d3d1511f0d2cb20b114ef269d0af430d12165
alfito234 <alfito.devaga@gmail.com>
2023-05-31 19:15:47 +0700

    fix again

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
     @include('partials.navbar')
-    <section>
+    <section id="post">
         <div class="bg-[#2B2B2B]">
             <div class="mx-auto max-w-2xl px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8">
                 <div class="grid grid-cols-1 items-start gap-x-8 gap-y-16 lg:grid-cols-2">
