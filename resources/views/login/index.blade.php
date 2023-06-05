d09dec9b6b1a0d783d8646e36748c3025ee98da3
alfito234 <alfito.devaga@gmail.com>
2023-06-05 11:32:58 +0700

    update login page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,91 +1,121 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="flex min-h-full flex-col justify-center pt-24 sm:px-6 lg:px-8 bg-[#2B2B2B]">
-        <div class="sm:mx-auto sm:w-full sm:max-w-md">
-            <img class="mx-auto h-20 w-auto" src="img/logo-vr.png" alt="VR Lab">
-            <h2 class="text-center text-2xl font-bold tracking-tight text-white">Sign in to your account
-            </h2>
+    <div class="flex min-h-screen">
+
+        <div class="relative flex-1 hidden w-0 lg:block">
+            <img class="absolute inset-0 object-cover w-full h-full"
+                src="https://images.unsplash.com/photo-1626379953822-baec19c3accd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+                alt="">
         </div>
-
-        <div class="mt-5 sm:mx-auto sm:w-full sm:max-w-[480px]">
-            <div class="bg-gray-600 px-6 py-12 shadow sm:rounded-lg sm:px-12">
-                <form class="space-y-6" action="#" method="POST">
-                    <div>
-                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email
-                            address</label>
-                        <div class="mt-2">
-                            <input id="email" name="email" type="email" autocomplete="email" required
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                        </div>
-                    </div>
-
-                    <div>
-                        <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                        <div class="mt-2">
-                            <input id="password" name="password" type="password" autocomplete="current-password" required
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                        </div>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center">
-                            <input id="remember-me" name="remember-me" type="checkbox"
-                                class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                            <label for="remember-me" class="ml-3 block text-sm leading-6 text-gray-900">Remember me</label>
-                        </div>
-
-                        <div class="text-sm leading-6">
-                            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot
-                                password?</a>
-                        </div>
-                    </div>
-
-                    <div>
-                        <button type="submit"
-                            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-                            in</button>
-                    </div>
-                </form>
-
-                <div>
-                    <div class="relative mt-10">
-                        <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                            <div class="w-full border-t border-gray-200"></div>
-                        </div>
-                        <div class="relative flex justify-center text-sm font-medium leading-6">
-                            <span class="bg-white px-6 text-gray-900">Or continue with</span>
-                        </div>
-                    </div>
-
-                    <div class="mt-6 grid grid-cols-2 gap-4">
-                        <a href="#"
-                            class="flex w-full items-center justify-center gap-3 rounded-md bg-[#1D9BF0] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#1D9BF0]">
-                            <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                                <path
-                                    d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                            </svg>
-                            <span class="text-sm font-semibold leading-6">Twitter</span>
-                        </a>
-
-                        <a href="#"
-                            class="flex w-full items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]">
-                            <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+        <div class="flex flex-col justify-center flex-1 px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+            @if (session()->has('success'))
+                <div class="p-4 rounded-md bg-green-50">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <svg class="w-5 h-5 text-green-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                 <path fill-rule="evenodd"
-                                    d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
                                     clip-rule="evenodd" />
                             </svg>
-                            <span class="text-sm font-semibold leading-6">GitHub</span>
-                        </a>
+                        </div>
+                        <div class="ml-3">
+                            <p class="text-sm font-medium text-green-800">{{ session('success') }}</p>
+                        </div>
+                        <div class="pl-3 ml-auto">
+                            <div class="-mx-1.5 -my-1.5">
+                                <button type="button"
+                                    class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50">
+                                    <span class="sr-only">Dismiss</span>
+                                    <svg class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path
+                                            d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
 
-            <p class="mt-10 text-center text-sm text-gray-500">
-                Not a member?
-                <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free
-                    trial</a>
-            </p>
+            @if (session()->has('loginError'))
+                <div class="p-4 rounded-md bg-yellow-50">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <svg class="w-5 h-5 text-red-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd"
+                                    d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <div class="ml-3">
+                            <div class="mt-2 text-sm text-red-700">
+                                <p>{{ session('loginError') }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="text-center alert alert-danger alert-dismissible fade show" role="alert">
+                    {{ session('loginError') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+
+            <div class="w-full max-w-sm mx-auto lg:w-96">
+                <div>
+                    <a href="/">
+                        <img class="w-auto h-20" src="img/logo-vr.png" alt="VR Lab Logo">
+                    </a>
+                    <h2 class="mt-0 text-2xl font-bold leading-9 tracking-tight text-white">Sign in to your account</h2>
+                </div>
+
+                <div class="mt-10">
+                    <div>
+                        <form action="/login" method="POST" class="space-y-6">
+                            @csrf
+                            <div>
+                                <label for="email" class="block text-sm font-medium leading-6 text-white">Email
+                                    address</label>
+                                <div class="">
+                                    <input id="email" name="email" type="email" autocomplete="email" required
+                                        class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                                </div>
+                                @error('email')
+                                    <div class="flex items-center mt-1 ml-1 text-xs font-medium tracking-wide text-red-500">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div>
+                                <label for="password"
+                                    class="block text-sm font-medium leading-6 text-white">Password</label>
+                                <div class="">
+                                    <input id="password" name="password" type="password" autocomplete="current-password"
+                                        required
+                                        class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                                </div>
+                                @error('password')
+                                    <div class="flex items-center mt-1 ml-1 text-xs font-medium tracking-wide text-red-500">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div>
+                                <small class="text-white">Don't have account? register <a href="/register"
+                                        class="underline decoration-solid hover:text-[#06E3FF]">here</a></small>
+                            </div>
+                            <div>
+                                <button type="submit"
+                                    class="flex w-full justify-center rounded-md bg-[#06E3FF] px-3 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-white/20 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+                                    in</button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
         </div>
     </div>
 
@@ -100,15 +130,15 @@
       @endif
 
       @if (session()->has('loginError'))
-      <div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
+      <div class="text-center alert alert-danger alert-dismissible fade show" role="alert">
         {{ session('loginError') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
       @endif
-        <main class="form-signin w-100 m-auto">
+        <main class="m-auto form-signin w-100">
             <form action="/login" method="POST">
               @csrf
-              <h1 class="h3 mb-3 fw-normal text-center">Login</h1>
+              <h1 class="mb-3 text-center h3 fw-normal">Login</h1>
               <div class="form-floating">
                 <input type="email" name="email" class="form-control @error('email')is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
                 <label for="email">Email address</label>
--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="flex min-h-screen dark">
+    <div class="flex min-h-screen">
         <div class="flex flex-col justify-center flex-1 px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
             <div class="w-full max-w-sm mx-auto lg:w-96">
                 <div>
-                    <img class="w-auto h-20" src="img/logo-vr.png" alt="VR Lab Logo">
+                    <a href="/">
+                        <img class="w-auto h-20" src="img/logo-vr.png" alt="VR Lab Logo">
+                    </a>
                     <h2 class="mt-0 text-2xl font-bold leading-9 tracking-tight text-white">Register your account</h2>
                 </div>
 
@@ -69,9 +71,13 @@
                             </div>
 
                             <div>
+                                <small class="text-white">Already have account? sign in <a href="/login"
+                                        class="underline decoration-solid hover:text-[#06E3FF]">here</a></small>
+                            </div>
+
+                            <div>
                                 <button type="submit"
-                                    class="flex w-full justify-center rounded-md bg-[#06E3FF] px-3 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-white/20 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-                                    in</button>
+                                    class="flex w-full justify-center rounded-md bg-[#06E3FF] px-3 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-white/20 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
                             </div>
                         </form>
                     </div>
@@ -85,85 +91,4 @@
                 alt="">
         </div>
     </div>
-
-    {{-- <div class="flex items-center justify-center px-6 py-12 ite items lg:px-8">
-        <div class="px-20 py-14 bg-[#2B2B2B] rounded-2xl drop-shadow-[0_0_20px_20px_rgba(1,1,1,0.2823529412)]">
-            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-                <img class="w-auto h-32 mx-auto" src="img/logo-vr.png" alt="VR Lab">
-                <h2 class="mt-10 text-2xl font-bold leading-9 tracking-tight text-center text-white">Register your account
-                </h2>
-            </div>
-
-            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form action="/register" method="POST" class="space-y-6">
-                    @csrf
-                    <div>
-                        <label for="name" class="block text-sm font-medium leading-6 text-white">Name
-                        </label>
-                        <div class="">
-                            <input id="name" name="name" type="text" required
-                                class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">
-                        </div>
-                        @error('name')
-                            <div class="flex items-center mt-1 ml-1 text-xs font-medium tracking-wide text-red-500">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-                    <div>
-                        <label for="username" class="block text-sm font-medium leading-6 text-white">username
-                        </label>
-                        <div class="">
-                            <input id="username" name="username" type="text" required
-                                class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">
-                        </div>
-                        @error('username')
-                            <div class="flex items-center mt-1 ml-1 text-xs font-medium tracking-wide text-red-500">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-                    <div>
-                        <label for="email" class="block text-sm font-medium leading-6 text-white">Email
-                            address</label>
-                        <div class="">
-                            <input id="email" name="email" type="email" autocomplete="email" required
-                                class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">
-                        </div>
-                        @error('email')
-                            <div class="flex items-center mt-1 ml-1 text-xs font-medium tracking-wide text-red-500">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-
-                    <div>
-                        <label for="password" class="block text-sm font-medium leading-6 text-white">Password</label>
-                        <div class="">
-                            <input id="password" name="password" type="password" autocomplete="current-password" required
-                                class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">
-                        </div>
-                        @error('password')
-                            <div class="flex items-center mt-1 ml-1 text-xs font-medium tracking-wide text-red-500">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-
-                    <div>
-                        <button type="submit"
-                            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-                            in</button>
-                    </div>
-                </form>
-
-                <p class="mt-10 text-sm text-center text-gray-400">
-                    Not a member?
-                    <a href="#" class="font-semibold leading-6 text-indigo-400 hover:text-indigo-300">Start a 14 day
-                        free
-                        trial</a>
-                </p>
-            </div>
-        </div>
-    </div> --}}
 @endsection
