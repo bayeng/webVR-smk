a352bd32e9f16bea315dfd1ffab916ac05c9454a
alfito234 <alfito.devaga@gmail.com>
2023-06-09 13:45:07 +0700

    fix categories

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -2,24 +2,30 @@
 
 @section('container')
     @include('partials.navbar')
-    <h1 class="mb-5">List Category</h1>
-    <div class="container">
-        <div class="row">
-            @foreach ($categories as $category)
-                <div class="col-md-4">
-                    <div class="card text-bg-dark">
-                        <img src="https://source.unsplash.com/1200x400?{{ $category->name }}" class="card-img" alt="...">
-                        <div class="card-img-overlay">
-                            <div class="position-absolute px-2 py-1" style="background-color: rgba(0,0,0,0.7)">
-                                <a class="text-decoration-none text-white" href="blog?category={{ $category->slug }}">
-                                    <h5 class="card-title">{{ $category->name }}</h5>
-                                </a>
+    <section id="category" class="text-white">
+        <div class="container">
+            <div class="relative overflow-hidden pt-14">
+                <div class="flex p-20 items">
+                    <h1>"Unveiling the Design Spectrum Categories in the World of Design"</h1>
+                    @foreach ($categories as $category)
+                        <div class="col-md-4">
+                            <div class="card text-bg-dark">
+                                <img src="https://source.unsplash.com/1200x400?{{ $category->name }}" class="card-img"
+                                    alt="...">
+                                <div class="card-img-overlay">
+                                    <div class="px-2 py-1 position-absolute" style="background-color: rgba(0,0,0,0.7)">
+                                        <a class="text-white text-decoration-none"
+                                            href="blog?category={{ $category->slug }}">
+                                            <h5 class="card-title">{{ $category->name }}</h5>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
-            @endforeach
+            </div>
         </div>
-    </div>
+    </section>
     @include('partials.footer')
 @endsection
