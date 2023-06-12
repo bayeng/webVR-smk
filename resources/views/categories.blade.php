34e35ba8928170e8aa8d2f33214aaebdbdaabe5c
alfito234 <alfito.devaga@gmail.com>
2023-06-12 13:29:49 +0700

    Revert "fix categories again"
    This reverts commit f0fdaa20ad0fd2c66d99e1288881c5d9412dd7dc.

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -4,20 +4,15 @@
     @include('partials.navbar')
     <section id="category" class="text-white">
         <div class="container">
-            <div class="flex justify-center overflow-hidden pt-14">
-                <div class="flex flex-col items-center px-10 py-5 md:p-20">
-                    <h1 class="max-w-6xl text-3xl font-semibold text-center md:text-5xl">"Unveiling the Design Spectrum
-                        Categories in
-                        the
+            <div class="relative overflow-hidden pt-14">
+                <div class="flex flex-col items-center p-20">
+                    <h1 class="max-w-6xl text-5xl font-semibold text-center">"Unveiling the Design Spectrum Categories in the
                         World of
                         Design"</h1>
                     <div
                         class="grid max-w-2xl grid-cols-1 mx-auto mt-16 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                         @foreach ($categories as $category)
                             <div class="flex items-start">
-
-                            </div>
-                            {{-- <div class="flex items-start">
                                 <div class="relative w-full">
                                     <img src="https://source.unsplash.com/1200x400?{{ $category->name }}"
                                         class="aspect-[16/9] w-full bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
@@ -31,7 +26,7 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div> --}}
+                            </div>
                         @endforeach
                     </div>
                 </div>
