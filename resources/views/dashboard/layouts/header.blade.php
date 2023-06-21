49aa45cba9c69d4001f83698d32c54b17ef79408
bayeng <bayurollins.87@gmail.com>
2023-06-21 11:15:28 +0700

    Update header.blade.php

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <header class="p-0 shadow navbar navbar-dark sticky-top bg-dark flex-md-nowrap">
     <a class="px-3 navbar-brand col-md-3 col-lg-2 me-0" href="{{ route('home') }}">
-        <img src="img/logo-vr.png" style="width:64px; height:64px; " alt="VR Lab"></a>
+        <img src="{{asset('/img/logo-vr.png')}}" style="width:64px; height:64px; " alt="VR Lab"></a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
         data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
