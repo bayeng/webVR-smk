7de313edfeda8338c60fb6d920327bc1df33d13f
bayeng <88835193+bayeng@users.noreply.github.com>
2024-03-28 03:36:00 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,14 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
+
         User::create([
             'name'=>'Bayu Krisna',
             'username'=>'bayukrisna',
             'email'=>'bayu@gmail.com',
             'password'=>bcrypt('kunam')
         ]);
-        
+
         User::factory(2)->create();
         // User::create([
         //     'name'=>'kunam manuk',
@@ -33,50 +33,35 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         Category::create([
-            'name'=>'Progamming',
-            'slug'=>'progamming',
+            'name'=>'Virtual Reality',
+            'slug'=>'virtual-reality',
 
         ]);
 
         Category::create([
-            'name'=>'Web Design',
-            'slug'=>'web-design',
-            
-        ]);
-        Category::create([
-            'name'=>'Personal',
-            'slug'=>'personal',
-            
-        ]);
-        Category::create([
-            'name'=>'Backend',
-            'slug'=>'Backend',
-            
-        ]);
-        Category::create([
-            'name'=>'FrontEnd',
-            'slug'=>'FrontEnd',
-            
-        ]);
-        Category::create([
-            'name'=>'DevOps',
-            'slug'=>'DevOps',
-            
-        ]);
-
-        // Post::factory(20)->create();
-
-        Post::create([
-            'title'=>'judul pertama',
-            'slug'=>'judul-pertama',
-            'excerpt'=>'lorem ipsum tot vok',
-            'body'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam magnam, magni harum quae laboriosam obcaecati eveniet cumque fugiat impedit ea corrupti eius voluptates aliquid vitae quaerat? Consequatur, nobis reprehenderit quis quod vel recusandae perferendis consectetur libero, enim rerum culpa, eum voluptatum expedita eaque sapiente corrupti ea nam possimus? Error, eveniet!',
-            'category_id'=>1,
-            'slogan'=>'jaya jaya luar biasa',
-            'user_id'=>1,
-            'video'=>'https://www.youtube.com/embed/yUKRkPKg5_U'
+            'name'=>'Metaverse',
+            'slug'=>'metaverse',
 
         ]);
+        Category::create([
+            'name'=>'Internet Of Things',
+            'slug'=>'internet-of-things',
+
+        ]);
+
+         Post::factory(1000)->create();
+
+//        Post::create([
+//            'title'=>'judul pertama',
+//            'slug'=>'judul-pertama',
+//            'excerpt'=>'lorem ipsum tot vok',
+//            'body'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam magnam, magni harum quae laboriosam obcaecati eveniet cumque fugiat impedit ea corrupti eius voluptates aliquid vitae quaerat? Consequatur, nobis reprehenderit quis quod vel recusandae perferendis consectetur libero, enim rerum culpa, eum voluptatum expedita eaque sapiente corrupti ea nam possimus? Error, eveniet!',
+//            'category_id'=>1,
+//            'slogan'=>'jaya jaya luar biasa',
+//            'user_id'=>1,
+//            'video'=>'https://www.youtube.com/embed/yUKRkPKg5_U'
+//
+//        ]);
 
         // Post::create([
         //     'title'=>'judul kedua',
@@ -95,6 +80,6 @@ class DatabaseSeeder extends Seeder
         //     'category_id'=>2,
         //     'user_id'=>2
         // ]);
-        
+
     }
 }
