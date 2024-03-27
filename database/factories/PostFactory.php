236a264264d0fbc0d8bf29619de99ce176d6ad95
bayeng <88835193+bayeng@users.noreply.github.com>
2024-03-28 04:10:23 +0700

    Update PostFactory.php

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -14,16 +14,22 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            'title'=>$this->faker->sentence(mt_rand(2,8)),
+//            'title'=>$this->faker->sentence(mt_rand(2,8)),
+            'title'=>'GAME MMORPG ADVANTURE',
             'slug'=>$this->faker->slug(),
             'excerpt'=>$this->faker->paragraph(),
             'body'=>collect($this->faker->paragraphs(mt_rand(5,10)))
                         ->map(fn($p)=> "<p>$p</p>")
                         ->implode(''),
             'user_id'=>mt_rand(1,3),
+            'image'=>'post-image/H4tdCIv6MN8KF19W4LLjqIhNITz5hrftmkVWU5J5.jpg',
+            'image2'=>'post-image/VxI3khjgMD19oTve8DUDjeouuj6nk436pc6LHylw.jpg',
+            'image3'=>'post-image/Okfep1Wg2l3vcXV7b8yjotitskKExKxRTkFQQy0m.jpg',
+            'image4'=>'post-image/UeAPPMsvH4FceWaoBj7FkiGr1HnGc2hwU1gC94JK.jpg',
+            'slogan'=>'TEMUKAN SENSASI LEBIH NYATA DENGAN GAME PETUALANGAN VIRTUAL REALITY',
             'category_id'=>mt_rand(1,2),
-            'video'=>'https://www.youtube.com/embed/yUKRkPKg5_U'
-            
+            'video'=>'https://www.youtube.com/embed/dNlbs9FVctU'
+
         ];
     }
 }
